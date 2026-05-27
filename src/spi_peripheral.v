9083e58b37cde87c098217ce58dd367539736dee
tianhaoyu2007 <tianhao.yu07@gmail.com>
2026-05-27 14:29:19 -0400

    Finished body of spi_peripheral module and synchronizer module

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -17,13 +17,35 @@ module spi_peripheral (
 
 );
 
-// Now we need to define the synchronizer to avoid metastability (since our clk and SCLK lines are asynchronous)
-reg [2:0] sclk_sync;
-reg [2:0] copi_sync;
-reg [2:0] ncs_sync;
+// Here, create synchronizers for each of the SPI input signals
+wire sclk_synced, sclk_rise, sclk_fall;
+wire copi_synced, copi_rise, copi_fall;
+wire ncs_synced,  ncs_rise,  ncs_fall;
 
+synchronizer #(.SYNC_STAGES(2)) sclk_sync_inst (
+    .async_in       (SCLK),
+    .clk            (clk),
+    .sync_out       (sclk_synced),
+    .rise_edge_tick (sclk_rise),
+    .fall_edge_tick (sclk_fall)
+);
 
+synchronizer #(.SYNC_STAGES(2)) copi_sync_inst (
+    .async_in       (COPI),
+    .clk            (clk),
+    .sync_out       (copi_synced),
+    .rise_edge_tick (copi_rise),
+    .fall_edge_tick (copi_fall)
+);
 
+    //synch for chip select just in case
+synchronizer #(.SYNC_STAGES(2)) ncs_sync_inst (
+    .async_in       (nCS),
+    .clk            (clk),
+    .sync_out       (ncs_synced),
+    .rise_edge_tick (ncs_rise),
+    .fall_edge_tick (ncs_fall)
+);
 
 endmodule
 
@@ -43,11 +65,12 @@ module synchronizer #(parameter SYNC_STAGES = 2)(
     reg [SYNC_STAGES-1:0] sync_regs = {SYNC_STAGES{1'b0}};  // init with all zeros
 
     always @(posedge clk) begin
-        sync_regs <= {sync_regs[SYNC_STAGES-2:0], async_in};    // shift left for every clock tick
+        sync_regs <= {sync_regs[SYNC_STAGES-2:0], async_in};    // shift left for every clock tick, basically moving through the regs
+
         sync_out <= sync_regs[SYNC_STAGES-1];
 
-        rise_edge_tick <= (sync_out != sync_regs[SYNC_STAGES-1]) & (sync_regs[SYNC_STAGES - 1] == T) ? T : F; 
-        fall_edge_tick <= (sync_out != sync_regs[SYNC_STAGES-1]) & (sync_regs[SYNC_STAGES - 1] == F) ? T : F; 
+        rise_edge_tick <= (sync_out != sync_regs[SYNC_STAGES-1]) & (sync_regs[SYNC_STAGES - 1] == T) ? T : F; // this is to check for if the edge just rose (from 0 to 1)
+        fall_edge_tick <= (sync_out != sync_regs[SYNC_STAGES-1]) & (sync_regs[SYNC_STAGES - 1] == F) ? T : F;  // this checks the converse, falling edge (1->0)
   
     end
 endmodule
